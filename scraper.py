d15e5aad7bb9c6b8790e31542602d2d216aab8b7
krynens <75901402+krynens@users.noreply.github.com>
2021-03-06 22:18:50 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,31 +8,22 @@ import scraperwiki
 
 today = datetime.today()
 
-for i in range(1, 50):
-    try:
-        print(f'Getting page {i}')
-        url = 'https://www.melbourne.vic.gov.au/building-and-development/property-information/planning-building-registers/Pages/town-planning-permits-register-search-results.aspx?AdvertisingOnly=on&page=' + str(i)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
+url = 'https://www.cardinia.vic.gov.au/advertisedplanningapplications'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'lxml')
 
-        table = soup.find('tbody')
-        rows = table.find_all('tr', class_='detail')
+table = soup.find('tbody')
+rows = table.find_all('tr')
 
-        for row in rows:
-            record = {}
-            record['address'] = row.find('td', class_='column2').text
-            date_received_raw = row.find('td', class_='column3').text
-            record['date_received'] = datetime.strptime(
-                date_received_raw, "%d/%m/%Y").strftime("%Y-%m-%d")
-            record['date_scraped'] = today.strftime("%Y-%m-%d")
-            record['description'] = row.find('td', class_='column4').text
-            record['council_reference'] = row.find('td', class_='column1').text
-            record['info_url'] = 'https://www.melbourne.vic.gov.au' + \
-                str(row.find('td', class_='column1')).split('"')[5]
+for row in rows:
+    record = {}
+    record['address'] = row.find_all('td')[2].text
+    record['date_scraped'] = today.strftime("%Y-%m-%d")
+    record['description'] = row.find_all('td')[1].text
+    record['council_reference'] = row.find_all('td')[0].text
+    record['info_url'] = str(row.find_all('td')[0]).split('"')[1]
+    on_notice_to_raw = row.find_all('td')[3].text
+    record['on_notice_to'] = datetime.strptime(on_notice_to_raw, "%d %B %Y").strftime("%Y-%m-%d")
 
-            scraperwiki.sqlite.save(
-                unique_keys=['council_reference'], data=record, table_name="data")
-
-    except:
-        print('Scraper finished.')
-        break
+    scraperwiki.sqlite.save(
+        unique_keys=['council_reference'], data=record, table_name="data")
